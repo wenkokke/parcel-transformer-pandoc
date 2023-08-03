3885c95f836fb60ddec4afe075b3ab3588ceee44
Wen Kokke <wenkokke@users.noreply.github.com>
2023-08-03 15:06:32 +0100

    Convert to TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,35 @@
-import { Transformer } from "@parcel/plugin";
-import ThrowableDiagnostic from "@parcel/diagnostic";
-import type { MutableAsset } from "@parcel/types";
-import { minimatch } from "minimatch";
-import type { MinimatchOptions } from "minimatch";
-import path from "path";
-import omit from "omit";
-import fs from "fs";
-import assert from "assert";
-import { execSync } from "child_process";
-import * as tmp from "tmp";
+import { Transformer } from '@parcel/plugin'
+import ThrowableDiagnostic from '@parcel/diagnostic'
+import type { MutableAsset } from '@parcel/types'
+import { minimatch } from 'minimatch'
+import type { MinimatchOptions } from 'minimatch'
+import path from 'path'
+import omit from 'omit'
+import fs from 'fs'
+import assert from 'assert'
+import { execSync } from 'child_process'
+import * as tmp from 'tmp'
 
-type PandocOptions = Partial<Record<string, any>>;
+type PandocOptions = Partial<Record<string, any>>
 
 interface PandocConfig {
-  root: string;
-  options: Partial<Record<string, PandocOptions>>;
+  root: string
+  options: Partial<Record<string, PandocOptions>>
 }
 
 // Apply a function to each string value in an object.
 // Recurse over arrays and objects.
 function mapValues<T>(fun: (val: string) => string, obj: any): typeof obj {
-  if (typeof obj === "string") {
-    return fun(obj);
+  if (typeof obj === 'string') {
+    return fun(obj)
   } else if (Array.isArray(obj)) {
-    return obj.map((val) => mapValues(fun, val));
-  } else if (typeof obj === "object") {
+    return obj.map((val) => mapValues(fun, val))
+  } else if (typeof obj === 'object') {
     return Object.fromEntries(
-      Object.entries(obj).map(([key, val]) => [key, mapValues(fun, val)])
-    );
+      Object.entries(obj).map(([key, val]) => [key, mapValues(fun, val)]),
+    )
   } else {
-    return obj;
+    return obj
   }
 }
 
@@ -38,32 +38,32 @@ function mapValues<T>(fun: (val: string) => string, obj: any): typeof obj {
 function isGlobMatch(
   filepath: string,
   pattern: string,
-  opts: Omit<MinimatchOptions, "matchBase"> = {}
+  opts: Omit<MinimatchOptions, 'matchBase'> = {},
 ) {
-  return minimatch(filepath, pattern, { ...(opts ?? {}), matchBase: true });
+  return minimatch(filepath, pattern, { ...(opts ?? {}), matchBase: true })
 }
 
 // Get the set of Pandoc CLI arguments for a given asset file path.
 function getOptions(filepath: string, config: PandocConfig) {
-  const relativeFilepath = path.relative(config.root, filepath);
+  const relativeFilepath = path.relative(config.root, filepath)
   const [_pattern, options] = Object.entries(config.options).find(
-    ([pattern, _options]) => isGlobMatch(relativeFilepath, pattern)
-  ) ?? [undefined, {}];
-  return Array.isArray(options) ? options : [options];
+    ([pattern, _options]) => isGlobMatch(relativeFilepath, pattern),
+  ) ?? [undefined, {}]
+  return Array.isArray(options) ? options : [options]
 }
 
 // Get the Pandoc reader for a given set of Pandoc CLI arguments and asset type.
 function getReader(options: PandocOptions, type: string) {
   if (options.from !== undefined) {
-    return options.from;
+    return options.from
   } else if (options.read !== undefined) {
-    return options.read;
+    return options.read
   } else {
     switch (type) {
-      case "md":
-        return "markdown";
+      case 'md':
+        return 'markdown'
       default:
-        return type;
+        return type
     }
   }
 }
@@ -71,20 +71,20 @@ function getReader(options: PandocOptions, type: string) {
 // Get the Pandoc writer for a given set of Pandoc CLI arguments.
 function getWriter(options: PandocOptions) {
   if (options.to !== undefined) {
-    return options.to;
+    return options.to
   } else if (options.write != undefined) {
-    return options.write;
+    return options.write
   } else {
-    return "html";
+    return 'html'
   }
 }
 
 // Get the asset type for a given Pandoc writer.
 function getAssetType(writer: string) {
   if (writer.match(/commonmark|gfm|markdown/)) {
-    return "md";
+    return 'md'
   } else {
-    return writer;
+    return writer
   }
 }
 
@@ -92,88 +92,88 @@ function getAssetType(writer: string) {
 function addAssetDependencies(asset: MutableAsset, options: PandocOptions) {
   return mapValues((filePath) => {
     if (fs.existsSync(filePath)) {
-      assert(typeof filePath === "string");
-      asset.invalidateOnFileChange(filePath);
+      assert(typeof filePath === 'string')
+      asset.invalidateOnFileChange(filePath)
     }
-    return filePath;
-  }, options);
+    return filePath
+  }, options)
 }
 
 // Render Pandoc CLI options.
 function renderOptions(options: PandocOptions) {
   return Object.entries(options).flatMap(([optionName, optionValue]) => {
-    if (typeof optionValue === "boolean") {
-      return [`--${optionName}`];
-    } else if (typeof optionValue === "string") {
-      return [`--${optionName}=${optionValue}`];
+    if (typeof optionValue === 'boolean') {
+      return [`--${optionName}`]
+    } else if (typeof optionValue === 'string') {
+      return [`--${optionName}=${optionValue}`]
     } else if (Array.isArray(optionValue)) {
       return optionValue.map((optionValue) => {
-        return `--${optionName}=${optionValue}`;
-      });
-    } else if (typeof optionValue === "object") {
+        return `--${optionName}=${optionValue}`
+      })
+    } else if (typeof optionValue === 'object') {
       return Object.entries(optionValue).map(
         ([variableName, variableValue]) => {
-          assert(typeof variableValue === "string");
-          return `${optionName} ${variableName}=${variableValue}`;
-        }
-      );
+          assert(typeof variableValue === 'string')
+          return `${optionName} ${variableName}=${variableValue}`
+        },
+      )
     }
-  });
+  })
 }
 
 export default new Transformer({
   async loadConfig({ config, logger }): Promise<PandocConfig> {
     // Load configuration from .pandocrc.
-    const configResult = await config.getConfig([".pandocrc"]);
+    const configResult = await config.getConfig(['.pandocrc'])
     if (configResult) {
-      const { contents, filePath } = configResult;
-      logger.verbose({ message: `Found ${filePath}` });
+      const { contents, filePath } = configResult
+      logger.verbose({ message: `Found ${filePath}` })
 
       // Replace '${.}' with path to .pandocrc.
-      const root = path.dirname(filePath);
+      const root = path.dirname(filePath)
       return {
         root,
-        options: mapValues((val) => val.replace("${.}", root).trim(), contents),
-      };
+        options: mapValues((val) => val.replace('${.}', root).trim(), contents),
+      }
     } else {
       throw new ThrowableDiagnostic({
         diagnostic: {
-          message: "Could not find .pandocrc",
-          origin: "parcel-transformer-pandoc",
+          message: 'Could not find .pandocrc',
+          origin: 'parcel-transformer-pandoc',
         },
-      });
+      })
     }
   },
   async transform({ asset, config, logger }) {
     // Determine the Pandoc CLI options.
-    const sequentialOptions = getOptions(asset.filePath, config);
+    const sequentialOptions = getOptions(asset.filePath, config)
 
     // Run Pandoc with each set of options.
     for (const options of sequentialOptions) {
       // Determine the Pandoc reader & writer.
-      const reader = getReader(options, asset.type);
-      const writer = getWriter(options);
+      const reader = getReader(options, asset.type)
+      const writer = getWriter(options)
 
       // Run Pandoc.
-      const input = await asset.getCode();
-      const logFile = tmp.fileSync({ prefix: "pandoc", postfix: ".json" });
+      const input = await asset.getCode()
+      const logFile = tmp.fileSync({ prefix: 'pandoc', postfix: '.json' })
 
       // Run Pandoc.
       const renderedOptions = renderOptions({
-        ...omit(["from", "to", "read", "write"], options),
+        ...omit(['from', 'to', 'read', 'write'], options),
         from: reader,
         to: writer,
         log: logFile.name,
-      });
-      const command = ["pandoc", ...renderedOptions].join(" ");
-      logger.verbose({ message: `Running '${command}'` });
+      })
+      const command = ['pandoc', ...renderedOptions].join(' ')
+      logger.verbose({ message: `Running '${command}'` })
       const output = execSync(command, {
         input,
         cwd: config.root,
-        encoding: "utf-8",
-      });
-      logger.verbose({ message: `Done` });
-      asset.setCode(output);
+        encoding: 'utf-8',
+      })
+      logger.verbose({ message: `Done` })
+      asset.setCode(output)
 
       // Add dependencies.
       // {
@@ -183,22 +183,22 @@ export default new Transformer({
       // "from": "templates/experimental-jams/page.html"
       // }
       const logEntries = JSON.parse(
-        fs.readFileSync(logFile.name, { encoding: "utf8" })
-      );
+        fs.readFileSync(logFile.name, { encoding: 'utf8' }),
+      )
       if (Array.isArray(logEntries)) {
         for (const logEntry of logEntries) {
           if (
-            typeof logEntry === "object" &&
-            logEntry.type === "LoadedResource"
+            typeof logEntry === 'object' &&
+            logEntry.type === 'LoadedResource'
           ) {
-            asset.invalidateOnFileChange(logEntry.from);
+            asset.invalidateOnFileChange(logEntry.from)
           }
         }
       }
 
       // Update the asset type based on the writer
-      asset.type = getAssetType(writer);
+      asset.type = getAssetType(writer)
     }
-    return [asset];
+    return [asset]
   },
-});
+})
